e39402ff70d53124a9829c3a21520f5a14829e4f
headlessdev <damian.bergemann@outlook.com>
2025-04-19 16:23:53 +0200

    small notification text fix

--- agent/main.go
+++ agent/main.go
@@ -206,7 +206,7 @@ func checkAndUpdateStatus(db *sql.DB, client *http.Client, apps []Application) {
 	var notificationTemplate string
 	err := db.QueryRow("SELECT notification_text FROM settings LIMIT 1").Scan(&notificationTemplate)
 	if err != nil || notificationTemplate == "" {
-		notificationTemplate = "The application '!name' (!url) went !status!"
+		notificationTemplate = "The application !name (!url) went !status!"
 	}
 
 	for _, app := range apps {
