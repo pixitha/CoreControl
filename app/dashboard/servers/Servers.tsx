0c9a7670fc095b389615251acf547faa69ab26e0
headlessdev <damian.bergemann@outlook.com>
2025-04-23 19:25:51 +0200

    Fix card spacing

--- app/dashboard/servers/Servers.tsx
+++ app/dashboard/servers/Servers.tsx
@@ -992,7 +992,7 @@ export default function Dashboard() {
                           </div>
                         </div>
                       </CardHeader>
-                      <div className="px-6 pb-6">
+                      <div className="px-6">
                         <NextLink href={`/dashboard/servers/${server.id}`}>
                           <Button variant="outline" className="w-full mt-2">
                             <History className="h-4 w-4 mr-2" />
