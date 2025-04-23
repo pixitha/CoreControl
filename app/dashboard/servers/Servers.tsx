6acba7172ce205219be49cec1ec67c63b1540809
headlessdev <damian.bergemann@outlook.com>
2025-04-23 19:49:51 +0200

    Add more spacing to the status indicator

--- app/dashboard/servers/Servers.tsx
+++ app/dashboard/servers/Servers.tsx
@@ -847,7 +847,7 @@ export default function Dashboard() {
                     >
                       <CardHeader>
                         {server.monitoring && (
-                          <div className="absolute top-2 right-2">
+                          <div className="absolute top-4 right-4">
                             <StatusIndicator isOnline={server.online} />
                           </div>
                         )}
